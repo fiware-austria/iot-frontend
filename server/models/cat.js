cd34c99ae691c9f4f4009ba509186bc7cc23ffb3
salho <peter.salhofer@fh-joanneum.at>
2017-09-15 09:08:11 +0200

    removed compiled typescript files

--- server/models/cat.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var catSchema = new mongoose.Schema({
-    name: String,
-    weight: Number,
-    age: Number
-});
-var Cat = mongoose.model('Cat', catSchema);
-exports["default"] = Cat;
