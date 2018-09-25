827f352f19864b7cfbf531b9225c64a93660063f
salho <peter.salhofer@fh-joanneum.at>
2018-09-25 16:20:12 +0200

    added expiration time of 8 hours to jwt token

--- server/controllers/user.ts
+++ server/controllers/user.ts
@@ -15,7 +15,7 @@ export default class UserCtrl extends BaseCtrl<IUserDocument> {
       if (!user) { return res.sendStatus(403); }
       user.comparePassword(req.body.password, (error, isMatch) => {
         if (!isMatch) { return res.sendStatus(403); }
-        const token = jwt.sign({ user: user }, process.env.SECRET_TOKEN); // , { expiresIn: 10 } seconds
+        const token = jwt.sign({ user: user }, process.env.SECRET_TOKEN, { expiresIn: '8h'}); // , { expiresIn: 10 } seconds
         res.status(200).json({ token: token });
       });
     });
