dc86a1df9a7df0b6bb01c0d2b9050cc88fe3fb37
Peter Salhofer <psalhofer@gmail.com>
2019-08-13 08:45:44 +0200

    dummy data installation scripts added

---
+++ scripts/devices.js
@@ -0,0 +1,474 @@
+"use strict";
+exports.__esModule = true;
+exports.devices = [
+    {
+        "entity_name": "Sensor1",
+        "entity_type": "static",
+        "device_id": "Dev_Sensor1",
+        "attributes": [{ "type": "Float", "name": "temperature", "object_id": "t" }, {
+                "type": "Float",
+                "name": "humidity",
+                "object_id": "h"
+            }, { "type": "Float", "name": "pm25", "object_id": "pm25" }, {
+                "type": "Float",
+                "name": "pm10",
+                "object_id": "pm10"
+            }, { "type": "Float", "name": "airPressure", "object_id": "ap" }, {
+                "type": "Float",
+                "name": "mangOHTemp",
+                "object_id": "t2"
+            }, { "type": "Float", "name": "mangOHPress", "object_id": "ap2" }]
+    },
+    {
+        "entity_name": "Sensor3",
+        "entity_type": "static",
+        "device_id": "Dev_Sensor3",
+        "attributes": [{ "type": "Float", "name": "temperature", "object_id": "t" }, {
+                "type": "Float",
+                "name": "humidity",
+                "object_id": "h"
+            }, { "type": "Float", "name": "pm25", "object_id": "pm25" }, {
+                "type": "Float",
+                "name": "pm10",
+                "object_id": "pm10"
+            }, { "type": "Float", "name": "airPressure", "object_id": "ap" }, {
+                "type": "Float",
+                "name": "mangOHTemp",
+                "object_id": "t2"
+            }, { "type": "Float", "name": "mangOHPress", "object_id": "ap2" }]
+    },
+    {
+        "entity_name": "Sensor6",
+        "entity_type": "static",
+        "device_id": "Dev_Sensor6",
+        "attributes": [{ "type": "Float", "name": "temperature", "object_id": "t" }, {
+                "type": "Float",
+                "name": "humidity",
+                "object_id": "h"
+            }, { "type": "Float", "name": "pm25", "object_id": "pm25" }, {
+                "type": "Float",
+                "name": "pm10",
+                "object_id": "pm10"
+            }, { "type": "Float", "name": "airPressure", "object_id": "ap" }, {
+                "type": "Float",
+                "name": "mangOHTemp",
+                "object_id": "t2"
+            }, { "type": "Float", "name": "mangOHPress", "object_id": "ap2" }]
+    },
+    {
+        "entity_name": "Sensor2",
+        "entity_type": "static",
+        "device_id": "Dev_Sensor2",
+        "attributes": [{ "type": "Float", "name": "temperature", "object_id": "t" }, {
+                "type": "Float",
+                "name": "humidity",
+                "object_id": "h"
+            }, { "type": "Float", "name": "pm25", "object_id": "pm25" }, {
+                "type": "Float",
+                "name": "pm10",
+                "object_id": "pm10"
+            }, { "type": "Float", "name": "airPressure", "object_id": "ap" }, {
+                "type": "Float",
+                "name": "mangOHTemp",
+                "object_id": "t2"
+            }, { "type": "Float", "name": "mangOHPress", "object_id": "ap2" }]
+    },
+    {
+        "entity_name": "Sensor5",
+        "entity_type": "static",
+        "device_id": "Dev_Sensor5",
+        "attributes": [{ "type": "Float", "name": "temperature", "object_id": "t" }, {
+                "type": "Float",
+                "name": "humidity",
+                "object_id": "h"
+            }, { "type": "Float", "name": "pm25", "object_id": "pm25" }, {
+                "type": "Float",
+                "name": "pm10",
+                "object_id": "pm10"
+            }, { "type": "Float", "name": "airPressure", "object_id": "ap" }, {
+                "type": "Float",
+                "name": "mangOHTemp",
+                "object_id": "t2"
+            }, { "type": "Float", "name": "mangOHPress", "object_id": "ap2" }]
+    },
+    {
+        "entity_name": "Sensor4",
+        "entity_type": "static",
+        "device_id": "Dev_Sensor4",
+        "attributes": [{ "type": "Float", "name": "temperature", "object_id": "t" }, {
+                "type": "Float",
+                "name": "humidity",
+                "object_id": "h"
+            }, { "type": "Float", "name": "pm25", "object_id": "pm25" }, {
+                "type": "Float",
+                "name": "pm10",
+                "object_id": "pm10"
+            }, { "type": "Float", "name": "airPressure", "object_id": "ap" }, {
+                "type": "Float",
+                "name": "mangOHTemp",
+                "object_id": "t2"
+            }, { "type": "Float", "name": "mangOHPress", "object_id": "ap2" }]
+    },
+    {
+        "entity_name": "Sensor7",
+        "entity_type": "static",
+        "device_id": "Dev_Sensor7",
+        "attributes": [{ "type": "Float", "name": "temperature", "object_id": "t" }, {
+                "type": "Float",
+                "name": "humidity",
+                "object_id": "h"
+            }, { "type": "Float", "name": "pm25", "object_id": "pm25" }, {
+                "type": "Float",
+                "name": "pm10",
+                "object_id": "pm10"
+            }, { "type": "Float", "name": "airPressure", "object_id": "ap" }, {
+                "type": "Float",
+                "name": "mangOHTemp",
+                "object_id": "t2"
+            }, { "type": "Float", "name": "mangOHPress", "object_id": "ap2" }]
+    },
+    {
+        "entity_name": "Sensor_DonBosco",
+        "entity_type": "static",
+        "device_id": "Dev_Sensor_DonBosco",
+        "attributes": [{ "type": "Float", "name": "temperature", "object_id": "t" }, {
+                "type": "Float",
+                "name": "humidity",
+                "object_id": "h"
+            }, { "type": "Float", "name": "pm1", "object_id": "pm1" }, {
+                "type": "Float",
+                "name": "pm25",
+                "object_id": "pm25"
+            }, { "type": "Float", "name": "pm10", "object_id": "pm10" }, {
+                "type": "Float",
+                "name": "no",
+                "object_id": "no"
+            }, { "type": "Float", "name": "no2", "object_id": "no2" }, {
+                "type": "Float",
+                "name": "so2",
+                "object_id": "so2"
+            }, { "type": "Float", "name": "co", "object_id": "co" }, {
+                "type": "Float",
+                "name": "o3",
+                "object_id": "o3"
+            }, { "type": "Float", "name": "airPressure", "object_id": "ap" }]
+    },
+    {
+        "entity_name": "Bus1_30",
+        "entity_type": "mobile",
+        "device_id": "Dev_Bus1_30",
+        "attributes": [{ "type": "geo:point", "name": "location", "object_id": "l" }, {
+                "type": "Float",
+                "name": "temperature",
+                "object_id": "t"
+            }, { "type": "Float", "name": "humidity", "object_id": "h" }, {
+                "type": "Float",
+                "name": "speed",
+                "object_id": "s"
+            }, { "type": "Float", "name": "pm1", "object_id": "pm1" }, {
+                "type": "Float",
+                "name": "pm25",
+                "object_id": "pm25"
+            }, { "type": "Float", "name": "pm10", "object_id": "pm10" }, {
+                "type": "Float",
+                "name": "no",
+                "object_id": "no"
+            }, { "type": "Float", "name": "no2", "object_id": "no2" }, {
+                "type": "Float",
+                "name": "airPressure",
+                "object_id": "ap"
+            }, { "type": "Float", "name": "vehicleId", "object_id": "vid" }, {
+                "type": "Float",
+                "name": "airQualityId",
+                "object_id": "aqid"
+            }]
+    },
+    {
+        "entity_name": "Bus2_31",
+        "entity_type": "mobile",
+        "device_id": "Dev_Bus2_31",
+        "attributes": [{ "type": "geo:point", "name": "location", "object_id": "l" }, {
+                "type": "Float",
+                "name": "temperature",
+                "object_id": "t"
+            }, { "type": "Float", "name": "humidity", "object_id": "h" }, {
+                "type": "Float",
+                "name": "speed",
+                "object_id": "s"
+            }, { "type": "Float", "name": "pm1", "object_id": "pm1" }, {
+                "type": "Float",
+                "name": "pm25",
+                "object_id": "pm25"
+            }, { "type": "Float", "name": "pm10", "object_id": "pm10" }, {
+                "type": "Float",
+                "name": "no",
+                "object_id": "no"
+            }, { "type": "Float", "name": "no2", "object_id": "no2" }, {
+                "type": "Float",
+                "name": "airPressure",
+                "object_id": "ap"
+            }, { "type": "Float", "name": "vehicleId", "object_id": "vid" }, {
+                "type": "Float",
+                "name": "airQualityId",
+                "object_id": "aqid"
+            }]
+    },
+    {
+        "entity_name": "Tram1_654",
+        "entity_type": "mobile",
+        "device_id": "Dev_Tram1_654",
+        "attributes": [{ "type": "geo:point", "name": "location", "object_id": "l" }, {
+                "type": "Float",
+                "name": "temperature",
+                "object_id": "t"
+            }, { "type": "Float", "name": "humidity", "object_id": "h" }, {
+                "type": "Float",
+                "name": "speed",
+                "object_id": "s"
+            }, { "type": "Float", "name": "pm1", "object_id": "pm1" }, {
+                "type": "Float",
+                "name": "pm25",
+                "object_id": "pm25"
+            }, { "type": "Float", "name": "pm10", "object_id": "pm10" }, {
+                "type": "Float",
+                "name": "no",
+                "object_id": "no"
+            }, { "type": "Float", "name": "no2", "object_id": "no2" }, {
+                "type": "Float",
+                "name": "airPressure",
+                "object_id": "ap"
+            }, { "type": "Float", "name": "vehicleId", "object_id": "vid" }, {
+                "type": "Float",
+                "name": "airQualityId",
+                "object_id": "aqid"
+            }]
+    },
+    {
+        "entity_name": "Tram2_655",
+        "entity_type": "mobile",
+        "device_id": "Dev_Tram2_655",
+        "attributes": [{ "type": "geo:point", "name": "location", "object_id": "l" }, {
+                "type": "Float",
+                "name": "temperature",
+                "object_id": "t"
+            }, { "type": "Float", "name": "humidity", "object_id": "h" }, {
+                "type": "Float",
+                "name": "speed",
+                "object_id": "s"
+            }, { "type": "Float", "name": "pm1", "object_id": "pm1" }, {
+                "type": "Float",
+                "name": "pm25",
+                "object_id": "pm25"
+            }, { "type": "Float", "name": "pm10", "object_id": "pm10" }, {
+                "type": "Float",
+                "name": "no",
+                "object_id": "no"
+            }, { "type": "Float", "name": "no2", "object_id": "no2" }, {
+                "type": "Float",
+                "name": "airPressure",
+                "object_id": "ap"
+            }, { "type": "Float", "name": "vehicleId", "object_id": "vid" }, {
+                "type": "Float",
+                "name": "airQualityId",
+                "object_id": "aqid"
+            }]
+    },
+    {
+        "entity_name": "Traffic1_west",
+        "entity_type": "traffic",
+        "device_id": "Dev_Traffic1_west",
+        "attributes": [{ "type": "Float", "name": "count", "object_id": "c" }, {
+                "type": "Float",
+                "name": "occupancy",
+                "object_id": "o"
+            }, { "type": "Float", "name": "speed", "object_id": "s" }]
+    },
+    {
+        "entity_name": "Traffic1_east",
+        "entity_type": "traffic",
+        "device_id": "Dev_Traffic1_east",
+        "attributes": [{ "type": "Float", "name": "count", "object_id": "c" }, {
+                "type": "Float",
+                "name": "occupancy",
+                "object_id": "o"
+            }, { "type": "Float", "name": "speed", "object_id": "s" }]
+    },
+    {
+        "entity_name": "Traffic2_east",
+        "entity_type": "traffic",
+        "device_id": "Dev_Traffic2_east",
+        "attributes": [{ "type": "Float", "name": "count", "object_id": "c" }, {
+                "type": "Float",
+                "name": "occupancy",
+                "object_id": "o"
+            }, { "type": "Float", "name": "speed", "object_id": "s" }]
+    },
+    {
+        "entity_name": "Traffic2_west",
+        "entity_type": "traffic",
+        "device_id": "Dev_Traffic2_west",
+        "attributes": [{ "type": "Float", "name": "count", "object_id": "c" }, {
+                "type": "Float",
+                "name": "occupancy",
+                "object_id": "o"
+            }, { "type": "Float", "name": "speed", "object_id": "s" }]
+    },
+    {
+        "entity_name": "Tram_1",
+        "entity_type": "test",
+        "device_id": "Dev_Tram_1",
+        "attributes": [{ "type": "Float", "name": "temperature", "object_id": "t" }, {
+                "type": "Text",
+                "name": "temp_sensor_info",
+                "object_id": "ti"
+            }, { "type": "Text", "name": "temp_sensor_status", "object_id": "ts" }, {
+                "type": "Float",
+                "name": "humidity",
+                "object_id": "h"
+            }, { "type": "Text", "name": "humid_sensor_info", "object_id": "hi" }, {
+                "type": "Text",
+                "name": "humid_sensor_status",
+                "object_id": "hs"
+            }, { "type": "geo:point", "name": "location", "object_id": "l" }, {
+                "type": "Text",
+                "name": "loc_sensor_info",
+                "object_id": "li"
+            }, { "type": "Text", "name": "loc_sensor_status", "object_id": "ls" }]
+    },
+    {
+        "entity_name": "Bus_1",
+        "entity_type": "test",
+        "device_id": "Dev_Bus_1",
+        "attributes": [{ "type": "Float", "name": "temperature", "object_id": "t" }, {
+                "type": "Text",
+                "name": "temp_sensor_info",
+                "object_id": "ti"
+            }, { "type": "Text", "name": "temp_sensor_status", "object_id": "ts" }, {
+                "type": "Float",
+                "name": "humidity",
+                "object_id": "h"
+            }, { "type": "Text", "name": "humid_sensor_info", "object_id": "hi" }, {
+                "type": "Text",
+                "name": "humid_sensor_status",
+                "object_id": "hs"
+            }, { "type": "geo:point", "name": "location", "object_id": "l" }, {
+                "type": "Text",
+                "name": "loc_sensor_info",
+                "object_id": "li"
+            }, { "type": "Text", "name": "loc_sensor_status", "object_id": "ls" }]
+    },
+    {
+        "entity_name": "Tram_2",
+        "entity_type": "test",
+        "device_id": "Dev_Tram_2",
+        "attributes": [{ "type": "Float", "name": "temperature", "object_id": "t" }, {
+                "type": "Text",
+                "name": "temp_sensor_info",
+                "object_id": "ti"
+            }, { "type": "Text", "name": "temp_sensor_status", "object_id": "ts" }, {
+                "type": "Float",
+                "name": "humidity",
+                "object_id": "h"
+            }, { "type": "Text", "name": "humid_sensor_info", "object_id": "hi" }, {
+                "type": "Text",
+                "name": "humid_sensor_status",
+                "object_id": "hs"
+            }, { "type": "geo:point", "name": "location", "object_id": "l" }, {
+                "type": "Text",
+                "name": "loc_sensor_info",
+                "object_id": "li"
+            }, { "type": "Text", "name": "loc_sensor_status", "object_id": "ls" }]
+    },
+    {
+        "entity_name": "Bus_2",
+        "entity_type": "test",
+        "device_id": "Dev_Bus_2",
+        "attributes": [{
+                "name": "temperature",
+                "object_id": "t",
+                "type": "Float"
+            }, {
+                "name": "temp_sensor_info",
+                "object_id": "ti",
+                "type": "Text"
+            }, { "name": "temp_sensor_status", "object_id": "ts", "type": "Text" }, {
+                "name": "humidity",
+                "object_id": "h",
+                "type": "Float"
+            }, {
+                "name": "humid_sensor_info",
+                "object_id": "hi",
+                "type": "Text"
+            }, { "name": "humid_sensor_status", "object_id": "hs", "type": "Text" }, {
+                "name": "location",
+                "object_id": "l",
+                "type": "geo:point"
+            }, {
+                "name": "loc_sensor_info",
+                "object_id": "li",
+                "type": "Text"
+            }, { "name": "loc_sensor_status", "object_id": "ls", "type": "Text" }]
+    },
+    {
+        "entity_name": "RasPi_1",
+        "entity_type": "static",
+        "device_id": "Dev_RasPi_1",
+        "attributes": [{ "type": "Float", "name": "temperature", "object_id": "t" }, {
+                "type": "Text",
+                "name": "temp_sensor_info",
+                "object_id": "ti"
+            }, { "type": "Text", "name": "temp_sensor_status", "object_id": "ts" }, {
+                "type": "Float",
+                "name": "humidity",
+                "object_id": "h"
+            }, { "type": "Text", "name": "humid_sensor_info", "object_id": "hi" }, {
+                "type": "Text",
+                "name": "humid_sensor_status",
+                "object_id": "hs"
+            }, { "type": "Float", "name": "pm25", "object_id": "pm25" }, {
+                "type": "Float",
+                "name": "pm10",
+                "object_id": "pm10"
+            }, { "type": "Text", "name": "air_sensor_info", "object_id": "ai" }, {
+                "type": "Text",
+                "name": "air_sensor_status",
+                "object_id": "as"
+            }]
+    },
+    {
+        "entity_name": "RasPi_2",
+        "entity_type": "static",
+        "device_id": "Dev_RasPi_2",
+        "attributes": [{ "type": "Float", "name": "temperature", "object_id": "t" }, {
+                "type": "Text",
+                "name": "temp_sensor_info",
+                "object_id": "ti"
+            }, { "type": "Text", "name": "temp_sensor_status", "object_id": "ts" }, {
+                "type": "Float",
+                "name": "humidity",
+                "object_id": "h"
+            }, { "type": "Text", "name": "humid_sensor_info", "object_id": "hi" }, {
+                "type": "Text",
+                "name": "humid_sensor_status",
+                "object_id": "hs"
+            }, { "type": "Float", "name": "pm25", "object_id": "pm25" }, {
+                "type": "Float",
+                "name": "pm10",
+                "object_id": "pm10"
+            }, { "type": "Text", "name": "air_sensor_info", "object_id": "ai" }, {
+                "type": "Text",
+                "name": "air_sensor_status",
+                "object_id": "as"
+            }]
+    },
+    {
+        "entity_name": "Test_1",
+        "entity_type": "test",
+        "device_id": "Dev_Test_1",
+        "attributes": [{ "type": "Float", "name": "temperature", "object_id": "t" }, {
+                "type": "Float",
+                "name": "humidity",
+                "object_id": "h"
+            }, { "type": "Float", "name": "pm25", "object_id": "pm25" }, { "type": "Float", "name": "pm10", "object_id": "pm10" }]
+    }
+];
---
+++ scripts/installDemoDevices.js
@@ -0,0 +1,49 @@
+"use strict";
+exports.__esModule = true;
+var querystring = require("querystring");
+var superagent = require("superagent");
+var iot_frontend_server = 'http://10.1.101.222:32111';
+// const iot_frontend_server = 'http://localhost:3000';
+var devices_1 = require("./devices");
+var token = "";
+var createDevices = function () {
+    return superagent.post(iot_frontend_server + '/iot/devices')
+        .send({ devices: devices_1.devices })
+        .set('Fiware-Service', 'graziot')
+        .set('Authorization', "Bearer " + token)
+        .set('Fiware-ServicePath', '/');
+};
+var services = {
+    "services": [
+        {
+            "apikey": "apistatic",
+            "entity_type": "test_sensor",
+            "resource": "/iot/d"
+        },
+        {
+            "apikey": "apimobile",
+            "entity_type": "mobile",
+            "resource": "/iot/d"
+        },
+        {
+            "apikey": "apitraffic",
+            "entity_type": "traffic",
+            "resource": "/iot/d"
+        }
+    ]
+};
+var createService = function () {
+    return Promise.resolve(console.log('Create Service: ' + iot_frontend_server + '/iot/services'))
+        .then(function () {
+        return superagent.post(iot_frontend_server + '/iot/services')
+            .send(services)
+            .set('Fiware-Service', 'graziot')
+            .set('Authorization', "Bearer " + token)
+            .set('Fiware-ServicePath', '/');
+    })
+        .then(function (r) { return console.log("Service Created!"); });
+};
+Promise.resolve()
+    .then(createService)
+    .then(createDevices)
+    .then(function () { return console.log("DONE!"); })["catch"](function (err) { return console.log(err); });
